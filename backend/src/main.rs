27826b1dc83c5242608ac84a896391fd8ae75ff5
clemetayer <c.lemetayer@laposte.net>
2024-06-09 17:52:14 +0200

    added a basic ci configuration for the backend

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,10 +1,10 @@
 mod api;
-mod handlers;
 mod domain;
+mod handlers;
 
 #[tokio::main]
 async fn main() {
     let routes = api::routes::build_routes();
     println!("Server started at http://localhost:8000");
-    warp::serve(routes).run(([127, 0, 0, 1], 8000)).await;
+    warp::serve(routes).run(([0, 0, 0, 0], 8000)).await;
 }
