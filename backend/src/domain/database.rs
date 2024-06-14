3d448a35e9918fa0eb694332f514fb57714d72b0
clemetayer <c.lemetayer@laposte.net>
2024-06-14 08:49:23 +0200

    refactor the connection to the database

--- backend/src/domain/database.rs
+++ backend/src/domain/database.rs
@@ -2,8 +2,8 @@ use backend::models::*;
 use backend::schema::collection_dependencies;
 use backend::schema::collections;
 use backend::*;
+use diesel::connection;
 use diesel::prelude::*;
-use diesel::sql_query;
 use diesel::SqliteConnection;
 
 use crate::domain::domain_models::CollectionDatabase;
@@ -11,20 +11,27 @@ use crate::domain::domain_models::CollectionDatabase;
 use super::domain_models::InitCollectionDatabase;
 use super::errors::DatabaseDomainError;
 
-pub fn init_collection(options: InitCollectionDatabase) -> Result<usize, DatabaseDomainError> {
-    match &mut establish_connection() {
-        Ok(connection) => {
-            match create_collection(connection, &options.name, &options.deezer_id, &options.url) {
-                Ok(res_size) => Ok(res_size),
-                Err(e) => {
-                    eprintln!("Error trying to save collection : {e}");
-                    Err(DatabaseDomainError::ResultError(e))
-                }
-            }
-        }
+fn get_connection() -> Result<SqliteConnection, DatabaseDomainError> {
+    match establish_connection() {
+        Ok(conn) => return Ok(conn),
         Err(e) => {
             eprintln!("Error trying connect to the database : {e}");
-            Err(DatabaseDomainError::ConnectionError())
+            return Err(DatabaseDomainError::ConnectionError());
+        }
+    }
+}
+
+pub fn init_collection(options: InitCollectionDatabase) -> Result<usize, DatabaseDomainError> {
+    match create_collection(
+        &mut get_connection()?,
+        &options.name,
+        &options.deezer_id,
+        &options.url,
+    ) {
+        Ok(res_size) => Ok(res_size),
+        Err(e) => {
+            eprintln!("Error trying to save collection : {e}");
+            Err(DatabaseDomainError::ResultError(e))
         }
     }
 }
@@ -48,41 +55,29 @@ fn create_collection(
 }
 
 pub fn get_collection_id_by_deezer_id(deezer_id: String) -> Result<i32, DatabaseDomainError> {
-    match &mut establish_connection() {
-        Ok(connection) => match collections::table
-            .filter(collections::deezer_id.eq(deezer_id.clone()))
-            .select(Collection::as_select())
-            .get_result(connection)
-        {
-            Ok(collection) => {
-                return Ok(collection.id);
-            }
-            Err(e) => {
-                eprintln!("Error getting the collection by deezer id {deezer_id} : {e}");
-                return Err(DatabaseDomainError::ResultError(e));
-            }
-        },
+    match collections::table
+        .filter(collections::deezer_id.eq(deezer_id.clone()))
+        .select(Collection::as_select())
+        .get_result(&mut get_connection()?)
+    {
+        Ok(collection) => {
+            return Ok(collection.id);
+        }
         Err(e) => {
-            eprintln!("Error trying connect to the database : {e}");
-            return Err(DatabaseDomainError::ConnectionError());
+            eprintln!("Error getting the collection by deezer id {deezer_id} : {e}");
+            return Err(DatabaseDomainError::ResultError(e));
         }
     }
 }
 
 pub fn list_collections() -> Result<Vec<CollectionDatabase>, DatabaseDomainError> {
-    match &mut establish_connection() {
-        Ok(connection) => match load_collections(connection) {
-            Ok(results) => {
-                return Ok(convert_collection_list_model_to_database(results));
-            }
-            Err(e) => {
-                eprintln!("Error trying to load the collections : {e}");
-                return Err(DatabaseDomainError::ResultError(e));
-            }
-        },
+    match load_collections(&mut get_connection()?) {
+        Ok(results) => {
+            return Ok(convert_collection_list_model_to_database(results));
+        }
         Err(e) => {
-            eprintln!("Error trying connect to the database : {e}");
-            Err(DatabaseDomainError::ConnectionError())
+            eprintln!("Error trying to load the collections : {e}");
+            return Err(DatabaseDomainError::ResultError(e));
         }
     }
 }
@@ -120,28 +115,22 @@ pub fn get_collection_with_tracks(
         "getting collection with tracks from deezer id : {}",
         deezer_id.clone()
     );
-    match &mut establish_connection() {
-        Ok(connection) => match collections::table
-            .filter(collections::deezer_id.eq(deezer_id.clone()))
-            .select(Collection::as_select())
-            .get_result(connection)
-        {
-            Ok(collection) => {
-                println!("getting tracks in collection");
-                return Ok(CollectionDatabase {
-                    deezer_id: collection.deezer_id,
-                    url: collection.url,
-                    name: collection.name,
-                });
-            }
-            Err(e) => {
-                eprintln!("Error getting the collection {} : {e}", deezer_id);
-                return Err(DatabaseDomainError::ResultError(e));
-            }
-        },
+    match collections::table
+        .filter(collections::deezer_id.eq(deezer_id.clone()))
+        .select(Collection::as_select())
+        .get_result(&mut get_connection()?)
+    {
+        Ok(collection) => {
+            println!("getting tracks in collection");
+            return Ok(CollectionDatabase {
+                deezer_id: collection.deezer_id,
+                url: collection.url,
+                name: collection.name,
+            });
+        }
         Err(e) => {
-            eprintln!("Error trying connect to the database : {e}");
-            return Err(DatabaseDomainError::ConnectionError());
+            eprintln!("Error getting the collection {} : {e}", deezer_id);
+            return Err(DatabaseDomainError::ResultError(e));
         }
     }
 }
@@ -150,30 +139,22 @@ pub fn add_collection_to_parent(
     parent_id: i32,
     child_id: i32,
 ) -> Result<bool, DatabaseDomainError> {
-    match &mut establish_connection() {
-        Ok(connection) => {
-            let collection_dependency = NewCollectionDependency {
-                parent_id: &parent_id,
-                child_id: &child_id,
-            };
-            match diesel::insert_into(collection_dependencies::table)
-                .values(&collection_dependency)
-                .on_conflict_do_nothing()
-                .execute(connection)
-            {
-                Ok(_) => {
-                    return Ok(true);
-                }
-                Err(e) => {
-                    eprintln!(
-                        "Error adding collection dependency : child id {child_id} to parent id {parent_id}: {e}"
-                    );
-                    return Err(DatabaseDomainError::ConnectionError());
-                }
-            }
+    let collection_dependency = NewCollectionDependency {
+        parent_id: &parent_id,
+        child_id: &child_id,
+    };
+    match diesel::insert_into(collection_dependencies::table)
+        .values(&collection_dependency)
+        .on_conflict_do_nothing()
+        .execute(&mut get_connection()?)
+    {
+        Ok(_) => {
+            return Ok(true);
         }
         Err(e) => {
-            eprintln!("Error trying connect to the database : {e}");
+            eprintln!(
+                "Error adding collection dependency : child id {child_id} to parent id {parent_id}: {e}"
+            );
             return Err(DatabaseDomainError::ConnectionError());
         }
     }
@@ -182,35 +163,25 @@ pub fn add_collection_to_parent(
 pub fn get_child_collections(
     parent_id: i32,
 ) -> Result<Vec<CollectionDatabase>, DatabaseDomainError> {
-    match &mut establish_connection() {
-        Ok(connection) => {
-            match collection_dependencies::table
-                .inner_join(
-                    collections::table.on(collections::id.eq(collection_dependencies::child_id)),
-                )
-                .filter(collection_dependencies::parent_id.eq(parent_id))
-                .select(Collection::as_select())
-                .get_results(connection)
-            {
-                Ok(collections) => {
-                    return Ok(collections
-                        .into_iter()
-                        .map(|collection| CollectionDatabase {
-                            deezer_id: collection.deezer_id,
-                            url: collection.url,
-                            name: collection.name,
-                        })
-                        .collect::<Vec<_>>());
-                }
-                Err(e) => {
-                    eprintln!("Error getting child collections : {e}");
-                    return Err(DatabaseDomainError::ResultError(e));
-                }
-            }
+    match collection_dependencies::table
+        .inner_join(collections::table.on(collections::id.eq(collection_dependencies::child_id)))
+        .filter(collection_dependencies::parent_id.eq(parent_id))
+        .select(Collection::as_select())
+        .get_results(&mut get_connection()?)
+    {
+        Ok(collections) => {
+            return Ok(collections
+                .into_iter()
+                .map(|collection| CollectionDatabase {
+                    deezer_id: collection.deezer_id,
+                    url: collection.url,
+                    name: collection.name,
+                })
+                .collect::<Vec<_>>());
         }
         Err(e) => {
-            eprintln!("Error trying connect to the database : {e}");
-            return Err(DatabaseDomainError::ConnectionError());
+            eprintln!("Error getting child collections : {e}");
+            return Err(DatabaseDomainError::ResultError(e));
         }
     }
 }
@@ -219,101 +190,79 @@ pub fn remove_collection_to_parent(
     parent_id: i32,
     child_id: i32,
 ) -> Result<bool, DatabaseDomainError> {
-    match &mut establish_connection() {
-        Ok(connection) => match diesel::delete(
-            collection_dependencies::table.filter(
-                collection_dependencies::parent_id
-                    .eq(parent_id)
-                    .and(collection_dependencies::child_id.eq(child_id)),
-            ),
-        )
-        .execute(connection)
-        {
-            Ok(_) => return Ok(true),
-            Err(e) => {
-                eprintln!("Error deleting collection dependencies : {e}");
-                return Err(DatabaseDomainError::ResultError(e));
-            }
-        },
+    match diesel::delete(
+        collection_dependencies::table.filter(
+            collection_dependencies::parent_id
+                .eq(parent_id)
+                .and(collection_dependencies::child_id.eq(child_id)),
+        ),
+    )
+    .execute(&mut get_connection()?)
+    {
+        Ok(_) => return Ok(true),
         Err(e) => {
-            eprintln!("Error trying connect to the database : {e}");
-            return Err(DatabaseDomainError::ConnectionError());
+            eprintln!("Error deleting collection dependencies : {e}");
+            return Err(DatabaseDomainError::ResultError(e));
         }
     }
 }
 
 pub fn remove_collection_in_database(collection_id: i32) -> Result<bool, DatabaseDomainError> {
-    match &mut establish_connection() {
-        Ok(connection) => {
-            let mut res = true;
-            match diesel::delete(
-                collection_dependencies::table
-                    .filter(collection_dependencies::parent_id.eq(collection_id)),
-            )
-            .execute(connection)
-            {
-                Ok(_) => res &= true,
-                Err(e) => {
-                    eprintln!("Error deleting collection dependencies : {e}");
-                    return Err(DatabaseDomainError::ResultError(e));
-                }
-            };
-            match diesel::delete(
-                collection_dependencies::table
-                    .filter(collection_dependencies::child_id.eq(collection_id)),
-            )
-            .execute(connection)
-            {
-                Ok(_) => res &= true,
-                Err(e) => {
-                    eprintln!("Error deleting collection dependencies : {e}");
-                    return Err(DatabaseDomainError::ResultError(e));
-                }
-            };
-            match diesel::delete(collections::table.filter(collections::id.eq(collection_id)))
-                .execute(connection)
-            {
-                Ok(_) => res &= true,
-                Err(e) => {
-                    eprintln!("Error deleting collection : {e}");
-                    return Err(DatabaseDomainError::ResultError(e));
-                }
-            };
-            return Ok(res);
-        }
+    let mut res = true;
+    let connection = &mut get_connection()?;
+    match diesel::delete(
+        collection_dependencies::table.filter(collection_dependencies::parent_id.eq(collection_id)),
+    )
+    .execute(connection)
+    {
+        Ok(_) => res &= true,
         Err(e) => {
-            eprintln!("Error trying connect to the database : {e}");
-            return Err(DatabaseDomainError::ConnectionError());
+            eprintln!("Error deleting collection dependencies : {e}");
+            return Err(DatabaseDomainError::ResultError(e));
         }
-    }
+    };
+    match diesel::delete(
+        collection_dependencies::table.filter(collection_dependencies::child_id.eq(collection_id)),
+    )
+    .execute(connection)
+    {
+        Ok(_) => res &= true,
+        Err(e) => {
+            eprintln!("Error deleting collection dependencies : {e}");
+            return Err(DatabaseDomainError::ResultError(e));
+        }
+    };
+    match diesel::delete(collections::table.filter(collections::id.eq(collection_id)))
+        .execute(connection)
+    {
+        Ok(_) => res &= true,
+        Err(e) => {
+            eprintln!("Error deleting collection : {e}");
+            return Err(DatabaseDomainError::ResultError(e));
+        }
+    };
+    return Ok(res);
 }
 
 pub fn domain_clear_database() -> Result<bool, DatabaseDomainError> {
-    match &mut establish_connection() {
-        Ok(connection) => {
-            match diesel::delete(collections::table).execute(connection) {
-                Ok(_) => {
-                    println!("collections cleared");
-                }
-                Err(e) => {
-                    eprintln!("Error clearing the collections : {:?}", e);
-                    return Err(DatabaseDomainError::ResultError(e));
-                }
-            };
-            match diesel::delete(collection_dependencies::table).execute(connection) {
-                Ok(_) => {
-                    println!("collection dependencies cleared");
-                }
-                Err(e) => {
-                    eprintln!("Error clearing the collection dependencies : {:?}", e);
-                    return Err(DatabaseDomainError::ResultError(e));
-                }
-            };
-            return Ok(true);
+    let connection = &mut get_connection()?;
+    match diesel::delete(collections::table).execute(connection) {
+        Ok(_) => {
+            println!("collections cleared");
         }
         Err(e) => {
-            eprintln!("Error trying connect to the database : {e}");
-            return Err(DatabaseDomainError::ConnectionError());
+            eprintln!("Error clearing the collections : {:?}", e);
+            return Err(DatabaseDomainError::ResultError(e));
         }
-    }
+    };
+    match diesel::delete(collection_dependencies::table).execute(connection) {
+        Ok(_) => {
+            println!("collection dependencies cleared");
+        }
+        Err(e) => {
+            eprintln!("Error clearing the collection dependencies : {:?}", e);
+            return Err(DatabaseDomainError::ResultError(e));
+        }
+    };
+    return Ok(true);
 }
